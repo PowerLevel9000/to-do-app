1fd023bd6b73e93a9a2b9ef51d2789febc960365
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-02-28 06:19:29 +0530

    innitialize components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import ToDoApp from './components/ToDoApp';
 
 function App() {
   return (
-    <div className="App" />
+    <ToDoApp />
   );
 }
 
---
+++ src/components/ToDoApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ToDoApp = () => (
+  <div>
+    <div>
+      <h1>Hello world!</h1>
+      <p>I am in a React Component!</p>
+    </div>
+  </div>
+);
+
+export default ToDoApp;
